17200df7698f2a965b22ee5c452229d800aeb0b0
lucymk <lucy.m.king92@gmail.com>
2018-12-30 14:09:34 +0000

    update response

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Response
 
 application = Flask(__name__)
 application.debug = True
@@ -6,7 +7,10 @@ application.debug = True
 
 @application.route('/', methods=['GET'])
 def hello():
-    return '<p>Hello World!</p>'
+    resp = Response("Foo bar baz")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
 
 if __name__ == "__main__":
     application.run()
