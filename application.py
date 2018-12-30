10f1cc28d62a5312e040e2d076a8d1936d294b53
lucymk <lucy.m.king92@gmail.com>
2018-12-30 15:04:57 +0000

    adds upload route with post method

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import Response
+from flask import (Flask, Response, request)
 
 application = Flask(__name__)
 application.debug = True
@@ -10,6 +9,23 @@ def hello():
     resp = Response("Foo bar baz")
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+
+@application.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            resp = Response("You didn't send the file.")
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
+
+        file = request.files['file']
+
+        print("file", file)
+
+        resp = Response("We got the file.")
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
 
 
 if __name__ == "__main__":
