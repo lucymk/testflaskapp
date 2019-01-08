39931abae55b7923619d39f824ec604e6aa8d06b
martingaston <squidman@gmail.com>
2019-01-08 15:41:53 +0000

    move download into url route

--- application.py
+++ application.py
@@ -54,6 +54,8 @@ def upload():
 
 @application.route("/airplane")
 def airplane():
+    path = os.path.join("downloads")
+    download("https://storage.googleapis.com/quickdraw_dataset/full/binary/airplane.bin", "airplane.bin", "downloads")
     path = os.path.join("downloads", "airplane.bin")
     abspath = os.path.abspath(path)
     if os.path.isfile(path):
@@ -71,6 +73,4 @@ def banjo():
 if __name__ == "__main__":
     # /opt/python/bundle/7/app/airplane.bin > default save location if no path
     # /opt/python/bundle/9/app/downloads/airplane.bin - is FALSE
-    path = os.path.join("downloads")
-    download("https://storage.googleapis.com/quickdraw_dataset/full/binary/airplane.bin", "airplane.bin", "downloads")
     application.run()
