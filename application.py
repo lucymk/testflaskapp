df6ebce93a748d4351947d8440624f4b4b2bece5
martingaston <squidman@gmail.com>
2019-01-09 11:40:19 +0000

    add get_dataset class

--- application.py
+++ application.py
@@ -1,57 +1,30 @@
+import os
 from flask import (Flask, Response, request, jsonify)
 from flask_cors import CORS
-
-import os
 from werkzeug.utils import secure_filename
-
 from image_convert import convert_to_base64
 from image_watermark import add_watermark
+from get_dataset import Dataset
 
-from test_download import download
-
-from pathlib import Path
-
-import six.moves.urllib as urllib
-import tarfile
 
 application = Flask(__name__)
 CORS(application)
 application.config.from_mapping(
     UPLOAD_FOLDER='/efs',
     MODEL_FOLDER='/efs/downloads/detection_models/ssd_mobilenet_v1_coco_2017_11_17',
-    DATASET_FOLDER='efs/downloads/drawing_dataset',
+    DATASET_FOLDER='/efs/downloads/drawing_dataset',
     ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg'])
 )
 application.debug = True
 
-for dir in [application.config["MODEL_FOLDER"], application.config["DATASET_FOLDER"]]:
-    if not Path(dir).exists():
-            Path(dir).mkdir(parents=True)
+dataset = Dataset(application.config['MODEL_FOLDER'],
+                  application.config["DATASET_FOLDER"], "categories.txt", logging=application.logger)
+dataset.check()
 
-if not os.path.isfile(os.path.join(application.config["MODEL_FOLDER"], "frozen_inference_graph.pb")):
-    url = "http://download.tensorflow.org/models/object_detection/"
-    filename = "ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
-    application.logger.info("Downloading model file: %s", filename)
-    opener = urllib.request.URLopener()
-    opener.retrieve(url + filename, filename)
-    tar_file = tarfile.open(filename)
-    for file in tar_file.getmembers():
-        file_name = os.path.basename(file.name)
-        if 'frozen_inference_graph.pb' in file_name:
-            tar_file.extract(file, path=str(Path(application.config["MODEL_FOLDER"]).parent))
-
-files = Path("downloads", "drawing_dataset").glob('*.bin')
-categories = [f.stem for f in files]
-
-if not categories:
-    category_list = ["aircraft carrier", "airplane", "alarm clock"]
-    source = "https://storage.googleapis.com/quickdraw_dataset/full/binary/"
-    for category in category_list:
-        application.logger.info("Downloading dataset file: %s", category)
-        download(source + category + ".bin", category + ".bin", application.config["DATASET_FOLDER"])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in application.config['ALLOWED_EXTENSIONS']
+
 
 @application.route('/', methods=['GET'])
 def hello():
@@ -83,15 +56,18 @@ def upload():
 
             return jsonify(status=200, base64=convert_to_base64(str(watermark_path)))
 
+
 @application.route("/bin/<filename>")
 def bin(filename=None):
     # print the current directory here
-    path = os.path.join(application.config["DATASET_FOLDER"], filename + ".bin")
+    path = os.path.join(
+        application.config["DATASET_FOLDER"], filename + ".bin")
     abspath = os.path.abspath(path)
     if os.path.isfile(path):
         return str(abspath)
     else:
-        return "FALSE " + str(abspath) 
+        return "FALSE " + str(abspath)
+
 
 if __name__ == "__main__":
     # /opt/python/bundle/7/app/airplane.bin > default save location if no path
---
+++ get_dataset.py
@@ -0,0 +1,74 @@
+import os
+import requests
+import tarfile
+from pathlib import Path
+import six.moves.urllib as urllib
+
+
+class Dataset(object):
+    def __init__(self, model_folder, dataset_folder, categories_list, logging):
+        self.model_folder = model_folder
+        self.dataset_folder = dataset_folder
+        self.categories_list = categories_list
+        self.logging = logging
+
+    def check(self):
+        self.logging.info(
+            "Checking for TensorFlow model and QuickDraw dataset...")
+        self.check_paths()
+        self.check_model()
+        self.check_binaries()
+
+    def check_paths(self):
+        for dir in [self.model_folder, self.dataset_folder]:
+            if not Path(dir).exists():
+                Path(dir).mkdir(parents=True)
+
+    def check_model(self):
+        if not os.path.isfile(os.path.join(self.model_folder, "frozen_inference_graph.pb")):
+            url = "http://download.tensorflow.org/models/object_detection/"
+            filename = "ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
+            self.logging.info("Downloading model file: %s", filename)
+            opener = urllib.request.URLopener()
+            opener.retrieve(url + filename, filename)
+            tar_file = tarfile.open(filename)
+            for file in tar_file.getmembers():
+                file_name = os.path.basename(file.name)
+                if 'frozen_inference_graph.pb' in file_name:
+                    tar_file.extract(file, path=str(
+                        Path(self.model_folder).parent))
+            os.remove(filename)
+
+    def check_binaries(self):
+        files = Path("downloads", "drawing_dataset").glob('*.bin')
+        categories = [f.stem for f in files]
+
+        if not categories:
+            category_list = self.read_categories()
+            source = "https://storage.googleapis.com/quickdraw_dataset/full/binary/"
+            for category in category_list:
+                self.logging.info(
+                    "Downloading dataset file: %s", category)
+                self.download(source + category + ".bin", category +
+                              ".bin", self.dataset_folder)
+
+    def read_categories(self):
+        with open(self.categories_list) as f:
+            categories = f.read().splitlines()
+        return categories
+
+    def download(self, url, filename, path):
+        """download file @ specified url and save it to path"""
+
+        if not Path(path).exists():
+            Path(path).mkdir(parents=True)
+
+        Path.touch(Path(path) / filename)
+        abspath = Path(path) / filename
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(str(abspath), 'wb') as f:
+                f.write(response.content)
+
+        return response.status_code
